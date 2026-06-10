a1e5091f4c0bf8de20cd9d4feee09ad1e3702879
Vini <visilva5690@hotmail.com>
2026-06-09 22:57:18 -0300

    (feat): Add Dockerfile, create DTO's and add Swagger

--- src/main/java/com/backend_challenge/controller/PasswordValidationController.java
+++ src/main/java/com/backend_challenge/controller/PasswordValidationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/password-validation")
+@RequestMapping("/api/password")
 public class PasswordValidationController {
 
     private final PasswordValidationService passwordValidationService;
--- src/main/java/com/backend_challenge/service/PasswordValidationService.java
+++ src/main/java/com/backend_challenge/service/PasswordValidationService.java
@@ -1,5 +1,7 @@
 package com.backend_challenge.service;
 
+import com.backend_challenge.dto.PasswordRequest;
+
 public interface PasswordValidationService {
 
     boolean isValid(PasswordRequest request);
